59ddf049980c28af9923f5db731c3dab5e46d6cd
Raphael Villas <raphael.villas@gmail.com>
2020-10-29 19:09:32 -0500

    Added trunc.go

--- hello.go
+++ hello.go
@@ -1,5 +1,17 @@
 package main
+
 import "fmt"
+
 func main() {
+	// You don't have to declare a type
+	// uninitialized values have a zero value (blank value for strings)
+	// var x, y int
+	// var myString string
+	// z := 200 // declare and assign a variable. Only able to use these in functions
+
+	// x = 50
+	// y = 100
+	// myString = "Have an amazing day!"
+
 	fmt.Printf("hello, world\n")
 }
