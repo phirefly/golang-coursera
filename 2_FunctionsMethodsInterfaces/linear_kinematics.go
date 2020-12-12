b6737dda850149416a630972c29d308e642f862a
Raphael Villas <raphael.villas@gmail.com>
2020-12-12 08:09:33 -0600

    ** Week 2: Submitted version

--- 2_FunctionsMethodsInterfaces/linear_kinematics.go
+++ 2_FunctionsMethodsInterfaces/linear_kinematics.go
@@ -1,37 +1,12 @@
-/*
-Let us assume the following formula for displacement s as a function of time t, acceleration a, initial velocity vo, and initial displacement so.
-
-s =½ a t2 + vot + so
-
-Write a program which first prompts the user to enter values for acceleration, initial velocity, and initial displacement. Then the program should prompt the user to enter a value for time and the program should compute the displacement after the entered time.
-
-You will need to define and use a function called GenDisplaceFn() which takes three float64 arguments, acceleration a, initial velocity vo, and initial displacement so. GenDisplaceFn() should return a function which computes displacement as a function of time, assuming the given values acceleration, initial velocity, and initial displacement. The function returned by GenDisplaceFn() should take one float64 argument t, representing time, and return one float64 argument which is the displacement travelled after time t.
-
-For example, let’s say that I want to assume the following values for acceleration, initial velocity, and initial displacement: a = 10, vo = 2, so = 1. I can use the following statement to call GenDisplaceFn() to generate a function fn which will compute displacement as a function of time.
-
-fn := GenDisplaceFn(10, 2, 1)
-
-
-Then I can use the following statement to print the displacement after 3 seconds.
-
-fmt.Println(fn(3))
-
-And I can use the following statement to print the displacement after 5 seconds.
-
-fmt.Println(fn(5))
-
-Submit your Go program source code.
-*/
-
 package main
 
 import "fmt"
 
 func main() {
-	// Prompt the user to enter values for acceleration, initial velocity, and initial displacement
-	var inputAcceleration string
-	var inputInitialVelocity string
-	var inputInitialDisplacement string
+	var inputAcceleration float64
+	var inputInitialVelocity float64
+	var inputInitialDisplacement float64
+	var inputTime float64
 
 	fmt.Print("Enter in a value for 'acceleration':")
 	fmt.Scan(&inputAcceleration)
@@ -44,20 +19,18 @@ func main() {
 	fmt.Println("initial velocity:", inputInitialVelocity)
 	fmt.Println("initial displacement:", inputInitialDisplacement)
 
-	GenDisplaceFn(10, 2, 1)
+	myFunc := GenDisplaceFn(inputAcceleration, inputInitialVelocity, inputInitialDisplacement)
 
-}
-
-func blah(t float64) float64 {
-	return 1.2345
+	fmt.Println("Your function has been created! Enter in a value for 'time' to use this function:")
+	fmt.Scan(&inputTime)
+	fmt.Println("Your result: ", myFunc(inputTime))
 }
 
 func GenDisplaceFn(a float64, vo float64, so float64) func(t float64) float64 {
-	fmt.Println("This is what you called this function with...")
-	fmt.Println("a:", a)
-	fmt.Println("vo:", vo)
-	fmt.Println("so:", so)
-
-	myFunc := blah
-	return myFunc
+	fn:= func(t float64) float64 {
+		return .5 * a * t * t +
+			vo * t +
+			so
+	}
+	return fn
 }
