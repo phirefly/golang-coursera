594c5e0a3d59e823f6a9fc6e7c615b762b9a869a
Raphael Villas <raphael.villas@gmail.com>
2020-12-16 14:12:45 -0600

    Ignore unused structs

--- 2_FunctionsMethodsInterfaces/week3_animals.go
+++ 2_FunctionsMethodsInterfaces/week3_animals.go
@@ -16,6 +16,12 @@ func main() {
 	// Make three methods called Eat(), Move(), and Speak(). The receiver type of all of your methods should be
 	// your Animal type. The Eat() method should print the animal’s food, the Move()  should print the
 	// animal’s locomotion, and the Speak()  should print the animal’s spoken sound.
+	cow := Animal{"grass", "walk", "moo"}
+	_ = cow
+	bird := Animal{"worms", "fly", "peep"}
+	_ = bird
+	snake := Animal{"mice", "slither", "hsss"}
+	_ = snake
 
 	var input string
 	fmt.Println("*** Welcome! Type in 'cow|bird|snake' and 'eat|move|speak'")
@@ -25,12 +31,17 @@ func main() {
 	input = bufscanner.Text()
 
 	parsedInput := strings.Split(input, " ")
+	//chosenAnimal :=
+	//chosenAction :=
 	fmt.Println("animal: ", parsedInput[0])
 	fmt.Println("action: ", parsedInput[1])
+	cow.Eat()
+	bird.Eat()
+	snake.Eat()
 }
 
-func Eat() {
-	fmt.Println("*** Eat")
+func (a Animal) Eat() {
+	fmt.Println("*** eats:", a.food)
 }
 
 func Move() {
