ae48bc84defd92fa9b0d92bda1a12009b5967ae8
Raphael Villas <raphael.villas@gmail.com>
2021-01-16 11:01:26 -0600

    *** getting cow to speak

--- 2_FunctionsMethodsInterfaces/week4_animals.go
+++ 2_FunctionsMethodsInterfaces/week4_animals.go
@@ -23,6 +23,18 @@ type Cow struct {
 	sound string
 }
 
+type Bird struct {
+	food string
+	locomotion string
+	sound string
+}
+
+type Snake struct {
+	food string
+	locomotion string
+	sound string
+}
+
 func (c Cow) Speak() {
 	fmt.Println("speaking: ", c.sound)
 }
@@ -33,9 +45,34 @@ func (c Cow) Eat() {
 	fmt.Println("eating: ", c.food)
 }
 
+func (b Bird) Speak() {
+	fmt.Println("speaking: ", b.sound)
+}
+func (b Bird) Move() {
+	fmt.Println("moving: ", b.locomotion)
+}
+func (b Bird) Eat() {
+	fmt.Println("eating: ", b.food)
+}
+
+func (s Snake) Speak() {
+	fmt.Println("speaking: ", s.sound)
+}
+func (s Snake) Move() {
+	fmt.Println("moving: ", s.locomotion)
+}
+func (s Snake) Eat() {
+	fmt.Println("eating: ", s.food)
+}
+
+
 func main() {
-	var cow1 = Cow{"grass", "walk", "moo"}
-	cow1.Speak()
+	//This doesn't work for some reason
+	//var myMap = make(map[string]types.Struct)
+	//myMap["cow"] = Cow{"grass", "walk", "moo"}
+
+	//myMap["bird"] = Bird{"grass", "walk", "moo"}
+	//myMap["snake"] = Snake{"grass", "walk", "moo"}
 
 	for {
 		var input string
@@ -48,25 +85,37 @@ func main() {
 
 		parsedInput := strings.Split(input, " ")
 		command := parsedInput[0]
-		chosenAnimal := parsedInput[1]
-		chosenAction := parsedInput[2]
+		//chosenAnimal := parsedInput[1]
+		//chosenAction := parsedInput[2]
 
 		switch command {
 		case "newanimal":
 			fmt.Println("--- new animal was chosen")
+
+			// CURRENT: take the entered animal type, and create a new instance of its
+			// corresponding struct
+			//var animal1 = myMap[chosenAnimal]
+			//animal1.Speak()
+			var a1 Animal
+			var myCow = Cow{"grass", "walk", "moo"}
+			a1 = myCow //Now the concrete type that a1 Animal is assigned to is myCow
+			_ = a1
+			myCow.Speak()
+
+
 		case "query":
 			fmt.Println("--- query was chosen")
 		default:
 			fmt.Println("--- action not known")
 		}
 
-		fmt.Println("*******")
-		fmt.Println("command: ", command)
-		fmt.Println("chosenAnimal: ", chosenAnimal)
-		fmt.Println("chosenAction: ", chosenAction)
-
-		fmt.Println("--- Created it!---")
-		fmt.Println("")
+		//fmt.Println("*******")
+		//fmt.Println("command: ", command)
+		//fmt.Println("chosenAnimal: ", chosenAnimal)
+		//fmt.Println("chosenAction: ", chosenAction)
+		//
+		//fmt.Println("--- Created it!---")
+		//fmt.Println("")
 
 		//meth := reflect.ValueOf(cow1[chosenAnimal]).MethodByName(strings.Title(chosenAction))
 		//meth.Call(nil)
